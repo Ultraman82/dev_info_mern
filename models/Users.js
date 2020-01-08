b21dfc5bbc9fee4cfabdcc139cf7f32bad474bc5
Ultraman82 <exelcior99@gmail.com>
2020-01-08 11:20:30 +0900

    d

--- models/Users.js
+++ models/Users.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
     name: {
         type: String,
-        required =  true
+        required: true
     },
     email: {
-        type: email,
+        type: String,
         required: true,
         unique: true
     },
