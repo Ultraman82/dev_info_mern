17f68f331faa9bca9c4b4d6b4785ed04c58391cd
Edgar <exelcior99@gmail.com>
2020-01-11 20:42:38 +0900

    Redux until Register

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import Alert from "./components/layout/Alert";
+
+// Redux
+import { Provider } from "react-redux";
+import store from "./store";
+
 import "./App.css";
 
-function App() {
-  return <div className="App">Readdssdgdct</div>;
-}
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar></Navbar>
+        <Route exact path="/" component={Landing}></Route>
+        <section className="container">
+          <Alert></Alert>
+          <Switch>
+            <Route exact path="/register" component={Register}></Route>
+            <Route exact path="/login" component={Login}></Route>
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
+);
 
 export default App;
