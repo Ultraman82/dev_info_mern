e37fac461d73b158310cc0fb29b85e98deb7095f
Edgar <exelcior99@gmail.com>
2020-01-14 13:55:13 +0900

    AddEducation profile

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,10 @@ import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Alert from "./components/layout/Alert";
 import Dashoboard from "./components/dashboard/Dashoboard";
+import CreateProfile from "./components/profile-forms/CreateProfile";
+import EditProfile from "./components/profile-forms/EditProfile";
+import AddExperience from "./components/profile-forms/AddExperience";
+import AddEducation from "./components/profile-forms/AddEducation";
 import PrivateRoute from "./components/routing/PrivateRoute";
 
 // Redux
@@ -39,6 +43,26 @@ const App = () => {
                 exact
                 path="/dashboard"
                 component={Dashoboard}
+              ></PrivateRoute>
+              <PrivateRoute
+                exact
+                path="/create-profile"
+                component={CreateProfile}
+              ></PrivateRoute>
+              <PrivateRoute
+                exact
+                path="/edit-profile"
+                component={EditProfile}
+              ></PrivateRoute>
+              <PrivateRoute
+                exact
+                path="/add-experience"
+                component={AddExperience}
+              ></PrivateRoute>
+              <PrivateRoute
+                exact
+                path="/add-education"
+                component={AddEducation}
               ></PrivateRoute>
             </Switch>
           </section>
